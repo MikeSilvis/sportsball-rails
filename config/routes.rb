67ac79c15e6792c505919965c8ad45cfa3284809
Mike Silvis <mikesilvis@gmail.com>
2014-10-30 23:09:01 -0700

    it works locally....

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do
-    resources :scores, only: :show
-    resources :teams, only: :show
-  end
-
   resources :scores, only: :show
 
   root to: 'scores#show'
