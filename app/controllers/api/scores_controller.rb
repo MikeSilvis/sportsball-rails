150520b2a768f6cbfa78daaa31c42ea8daf5f5c1
Mike Silvis <mikesilvis@gmail.com>
2014-11-01 18:10:17 -0700

    updating the time now call

--- app/controllers/api/scores_controller.rb
+++ app/controllers/api/scores_controller.rb
@@ -12,7 +12,7 @@ class Api::ScoresController < ApplicationController
   end
 
   def current_time
-    @current_time ||= (params[:date] ? Date.parse(params[:date]) : Date.today).in_time_zone("Eastern Time (US & Canada)").to_date
+    @current_time ||= (params[:date] ? Date.parse(params[:date]) : Time.now).in_time_zone("Eastern Time (US & Canada)").to_date
   end
 
   def scores
