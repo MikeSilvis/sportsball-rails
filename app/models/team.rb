4edc3a61e52304a312b64d1aa02385080bdfc3e2
Mike Silvis <mikesilvis@gmail.com>
2014-11-08 15:35:13 -0800

    adding logos!

--- app/models/team.rb
+++ app/models/team.rb
@@ -16,24 +16,25 @@ class Team
     ActionController::Base.helpers.image_url("#{league}-teams/#{data_name}.png")
   end
 
-  #concerning :DownloadingLogo do
-    #class_methods do
+  ## TODO: FIX / DELETE ME
+  concerning :DownloadingLogo do
       def self.score_data(array)
         array.each do |a|
           away_team = Team.new
+          away_team.league = a[:league]
           away_team.data_name = a[:away_team]
           away_team.download_logo(a[:away_team_logo])
 
           home_team = Team.new
+          home_team.league = a[:league]
           home_team.data_name = a[:home_team]
           home_team.download_logo(a[:home_team_logo])
         end
       end
-    #end
     def download_logo(url)
       open("app/assets/images/#{league}-teams/#{data_name}.png", 'wb') do |file|
         file << open(url).read
       end
     end
-  #end
+  end
 end
