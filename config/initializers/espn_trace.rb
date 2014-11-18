f5763282474bae39b4c28b062532583dc2e15fc2
Mike Silvis <mikesilvis@gmail.com>
2014-11-18 07:50:24 -0800

    adding methods tracer to explictly determine if nokogiri is slow or just the get request

---
+++ config/initializers/espn_trace.rb
@@ -0,0 +1,10 @@
+class ESPN
+  class << self
+    include ::NewRelic::Agent::MethodTracer
+
+    add_method_tracer :get, 'ESPN/get'
+
+    add_method_tracer :visitor_home_parse, 'ESPN/visitor_home_parse'
+    add_method_tracer :winner_loser_parse, 'ESPN/winner_loser_parse'
+  end
+end
