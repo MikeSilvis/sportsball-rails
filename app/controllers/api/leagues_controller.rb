8c31064ebf21cb479f65d96ed8fefaa7851fcbdf
Mike Silvis <mikesilvis@gmail.com>
2014-12-29 14:33:19 -0500

    temp updating league cache to expire now

--- app/controllers/api/leagues_controller.rb
+++ app/controllers/api/leagues_controller.rb
@@ -2,7 +2,8 @@ class Api::LeaguesController < ApplicationController
   skip_before_filter :ensure_valid_league
 
   def index
-    expires_in 7.days, public: true
+    #expires_in 7.days, public: true
+    expires_now
 
     render json: { leagues: League.all }
   end
