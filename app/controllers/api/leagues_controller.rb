51606827c0c44d520de7ab2a8f4fd0d49a6f1104
Mike Silvis <mikesilvis@gmail.com>
2014-12-29 14:43:13 -0500

    fixing the college requests

--- app/controllers/api/leagues_controller.rb
+++ app/controllers/api/leagues_controller.rb
@@ -2,8 +2,7 @@ class Api::LeaguesController < ApplicationController
   skip_before_filter :ensure_valid_league
 
   def index
-    #expires_in 7.days, public: true
-    expires_now
+    expires_in 7.days, public: true
 
     render json: { leagues: League.all }
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::API
+  before_filter :fix_college
   before_filter :ensure_valid_league
 
   def available_leagues
     @allowed_sports ||= ESPN.leagues
+  end
+
+  def fix_college
+    params[:league_id] = 'ncf' if params[:league_id] == 'college-football'
   end
 
   def ensure_valid_league
