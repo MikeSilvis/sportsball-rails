326c0aa51861a6e85b4314221f8d7432b0d3cb92
Mike Silvis <mikesilvis@gmail.com>
2014-12-28 23:37:32 -0500

    updating league controller header

--- app/controllers/api/leagues_controller.rb
+++ app/controllers/api/leagues_controller.rb
@@ -1,6 +1,9 @@
 class Api::LeaguesController < ApplicationController
   skip_before_filter :ensure_valid_league
+
   def index
+    expires_in 7.days, public: true
+
     render json: { leagues: League.all }
   end
 end
