d69c61870d252263f0c708ed7e8d267ac842dcec
Mike Silvis <mikesilvis@gmail.com>
2015-01-11 16:48:09 -0800

    updating gem, and fixing tests

---
+++ spec/controllers/api/schedules_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Api::SchedulesController, type: :controller, vcr: true do
+  describe 'GET :show' do
+    before { get :show, league_id: league, team_id: team_id }
+    context 'nhl' do
+      let(:league) { 'nhl' }
+      let(:team_id) { 'pit' }
+      it { expect(response).to have_http_status(:ok) }
+    end
+    context 'nfl' do
+      let(:league) { 'nfl' }
+      let(:team_id) { 'pit' }
+      it { expect(response).to have_http_status(:ok) }
+    end
+    context 'ncf' do
+      let(:league) { 'ncf' }
+      let(:team_id) { '213' }
+      it { expect(response).to have_http_status(:ok) }
+    end
+  end
+end
