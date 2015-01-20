6a677a224b42cfab016d5eb58684fec8941bf56d
Mike Silvis <mikesilvis@gmail.com>
2015-01-19 22:16:31 -0800

    adding golden state to the blurred headers

--- app/controllers/api/leagues_controller.rb
+++ app/controllers/api/leagues_controller.rb
@@ -2,8 +2,6 @@ class Api::LeaguesController < ApplicationController
   skip_before_filter :ensure_valid_league
 
   def index
-    expires_in 7.days, public: true
-
     render json: { leagues: League.all }
   end
 end
