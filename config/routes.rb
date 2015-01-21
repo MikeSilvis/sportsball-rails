92538d048433dfc536563fac607a210daf4944ed
Mike Silvis <mikesilvis@gmail.com>
2015-01-20 21:48:59 -0800

    removing API namespace cause this app is only an endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    get "images/:path" => Dragonfly.app.endpoint { |params, app|
-      size = params[:size] ? params[:size] : '70x70'
-      app.fetch_file("app/assets/images/#{params[:path]}.png").thumb(size)
-    }, as: 'image'
+  get "images/:path" => Dragonfly.app.endpoint { |params, app|
+    size = params[:size] ? params[:size] : '70x70'
+    app.fetch_file("app/assets/images/#{params[:path]}.png").thumb(size)
+  }, as: 'image'
 
-    resources :leagues, only: :index do
-      resources :scores, only: :index
-      resources :boxscores, only: :show
-      resources :previews, only: :show
-      resources :standings, only: :index
-      resources :teams, only: [] do
-        resource :schedule, only: :show
-      end
+  resources :leagues, only: :index do
+    resources :scores, only: :index
+    resources :boxscores, only: :show
+    resources :previews, only: :show
+    resources :standings, only: :index
+    resources :teams, only: [] do
+      resource :schedule, only: :show
     end
   end
 
