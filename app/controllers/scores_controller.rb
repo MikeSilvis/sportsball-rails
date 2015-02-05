192d6927398a2da92746cf1adce94ed498f3a4b2
Mike Silvis <mikesilvis@gmail.com>
2015-02-05 09:31:03 -0800

    adding the ability to specify a time zone

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -6,19 +6,19 @@ class ScoresController < ApplicationController
   private
 
   def current_time
-    @current_time ||= date.in_time_zone('Pacific Time (US & Canada)').to_date
+    @current_time ||= date.in_time_zone(time_zone).to_date
   end
 
   def date
-    @date ||= if params[:date]
-                Date.parse(params[:date])
-              else
-                Time.now
-              end
+    @date ||= params[:date] ? Date.parse(params[:date]) : Time.now
   end
 
   def scores
     @scores ||= league.scores(current_time.to_date)
+  end
+
+  def time_zone
+    params[:time_zone] ? params[:time_zone] : 'Pacific Time (US & Canada)'
   end
 
   def league
