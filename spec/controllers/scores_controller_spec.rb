f4adbb5c2b33263d71033c21e907b6e016a1ae23
Mike Silvis <mikesilvis@gmail.com>
2015-02-25 21:00:49 -0700

    fixing recap

--- spec/controllers/scores_controller_spec.rb
+++ spec/controllers/scores_controller_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 describe ScoresController, type: :controller, vcr: true do
   describe 'GET :index' do
-    before { get :index, league_id: league, date: Date.new(2014, 11, 13) }
+    before do
+      #allow_any_instance_of(Score).to receive(:query_with_timeout).and_return([])
+      get :index, league_id: league, date: Date.new(2014, 11, 13)
+    end
     context 'nhl' do
       let(:league) { 'nhl' }
       it { expect(response).to have_http_status(:ok) }
