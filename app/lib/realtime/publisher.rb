f0e6cca738bb855bc654f405e561ca9028e52013
Mike Silvis <mikesilvis@gmail.com>
2015-03-23 12:00:23 -0700

    small refactor

--- app/lib/realtime/publisher.rb
+++ app/lib/realtime/publisher.rb
@@ -3,22 +3,31 @@ require 'pusher'
 class Realtime::Publisher
   Pusher.url = ENV['JUMBOTRON_PUSHER_URL']
 
-  def self.send(league, game_id)
+  def self.send_boxscore(league, game_id)
     game_id = game_id.to_s
+    boxscore = Boxscore.find(league, game_id)
 
-    Pusher["boxscore_#{league}_#{game_id}"].trigger('event', {
-      boxscore: Boxscore.find(league, game_id),
-      game: League.new(league).scores(ActiveSupport::TimeZone['America/New_York'].today).detect { |s| s.boxscore == game_id }
+    push_event("boxscore_#{league}_#{game_id}", {
+      boxscore: boxscore,
+      game: League.new(league).scores(boxscore.game_date.to_date).detect { |s| s.boxscore == game_id }
     })
   end
 
-  def self.send_scores(league)
-    Pusher["scores_#{league}"].trigger('event', {
-      scores: League.new(league).scores(ActiveSupport::TimeZone['America/New_York'].today)
+  def self.send_scores(league, date)
+    push_event("scores_#{league}_#{date.strftime('%Y-%m-%d')}", {
+      scores: League.new(league).scores(date)
     })
+  end
+
+  def self.push_event(channel, data)
+    client.trigger(channel, 'event', data)
   end
 
   def today
     ActiveSupport::TimeZone['America/New_York'].today
+  end
+
+  def self.client
+    Pusher
   end
 end
