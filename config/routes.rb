27c18084d7a320c07ceff35ed10d5b6dd8fba32d
Mike Silvis <mikesilvis@gmail.com>
2015-03-24 14:26:31 -0700

    fixing image route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   resources :realtime_webhook, only: :create
+  get 'images/:path' => redirect('not_found'), as: 'image'
 
   resources :leagues, only: :index do
+
     resources :image_downloader, only: [:update, :show, :index] do
       get :get_update
     end
