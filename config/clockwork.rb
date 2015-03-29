d705f2fcf670a142464673fc342d35626f0a3cf7
Mike Silvis <mikesilvis@gmail.com>
2015-03-29 15:11:08 -0700

    removing clockwork

--- config/clockwork.rb
+++
@@ -1,8 +0,0 @@
-require 'clockwork'
-require './config/boot'
-require './config/environment'
-
-module Clockwork
-  #every(1.minute, 'frequent.job') { RealtimePush.perform_async if Realtime::Channel.any? }
-  #every(7.day, 'cache.clear') { Precache.perform_async }
-end
