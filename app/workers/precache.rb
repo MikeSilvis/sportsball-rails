e99ed8bdf3e4411ca3b4fe430644a7fa9999e0fb
Mike Silvis <mikesilvis@gmail.com>
2015-03-29 15:11:35 -0700

    completely removing sidekiq from the main app

--- app/workers/precache.rb
+++
@@ -1,8 +0,0 @@
-class Precache
-  include Sidekiq::Worker
-
-  def perform
-    Rails.cache.clear
-    ESPN::Cache.precache
-  end
-end
--- app/workers/realtime_push.rb
+++
@@ -1,7 +0,0 @@
-class RealtimePush
-  include Sidekiq::Worker
-
-  def perform
-    Realtime::Checker.push_updates
-  end
-end
