e1d6aec858b6eb747d8b554ffae3b6fc79814f24
Mike Silvis <mikesilvis@gmail.com>
2015-05-03 10:46:14 -0700

    trying again to remove cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   before_filter :fix_college
   before_filter :ensure_valid_league
+  after_filter  :remove_cookies
 
   def available_leagues
     @allowed_sports ||= ESPN.leagues
@@ -13,5 +14,9 @@ class ApplicationController < ActionController::API
 
   def ensure_valid_league
     fail ActionController::RoutingError.new('Not Found') unless available_leagues.include?(params[:league_id])
+  end
+
+  def remove_cookies
+    request.session_options[:skip] = true
   end
 end
--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,7 @@
 require 'raven'
 
-Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN']
+if ENV['SENTRY_DSN']
+  Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
+  end
 end
