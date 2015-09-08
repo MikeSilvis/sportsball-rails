2c4b18a2ddc3361098b72aac94762ea7b712633f
Mike Silvis <mikesilvis@gmail.com>
2015-09-07 21:24:43 -0700

    fucking a

--- app/models/boxscore.rb
+++ app/models/boxscore.rb
@@ -98,7 +98,7 @@ class Boxscore < QueryBase
 
     total_rows = @score_summary.first.count
 
-    @score_summary = (@score_summary << ['', (1..total_rows).to_a.map(&:to_s), event.status.detail].flatten).reverse
+    @score_summary = (@score_summary << ['', (1..(total_rows-1)).to_a.map(&:to_s), event.status.detail].flatten).reverse
 
     return @score_summary
   end
